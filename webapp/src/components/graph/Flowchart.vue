d2c61863d4496545088997d2fbf7a2919cb81d14
Florian Becker <florian00m.becker@gmail.com>
2023-05-05 17:46:19 +0000

    added connection creation and label changing

--- webapp/src/components/graph/Flowchart.vue
+++ webapp/src/components/graph/Flowchart.vue
@@ -2,34 +2,47 @@
 <script lang="ts" setup>
 import { Background, BackgroundVariant } from '@vue-flow/background';
 import { Controls } from '@vue-flow/controls';
-import { Edge, VueFlow } from '@vue-flow/core';
+import { Connection, Edge, GraphNode, NodeMouseEvent, useVueFlow, VueFlow } from '@vue-flow/core';
 import { ref } from 'vue';
 import { Dialog } from '../modals';
 import { edges as initialEdges, multiSelectKeys, nodes as initialNodes, nodeTypes } from './config';
-import type { CustomNode } from './Types';
 
 const dialog = ref<typeof Dialog | null>(null);
+const selectedNode = ref<number>(0);
+const nodeDataIndex = ref<string>('name');
 
-const nodes = ref<Array<CustomNode>>(initialNodes);
-const edges = ref<Array<Edge>>(initialEdges);
+const { nodes, edges, addEdges } = useVueFlow({
+    edges: initialEdges,
+    nodes: initialNodes,
+    nodeTypes: nodeTypes,
+    multiSelectionKeyCode: multiSelectKeys,
+});
 
-nodes.value?.forEach(
-    element =>
-        (element.events = {
-            doubleClick: () => {
-                console.log('double clicked');
-            },
-        }),
-);
+const onConnect = (connection: Connection) => {
+    const edge = connection as Edge;
+    edge.style = {
+        strokeWidth: 4,
+    };
+    addEdges([edge]);
+};
+
+const onNodeDoubleClick = (event: NodeMouseEvent) => {
+    selectedNode.value = nodes.value.findIndex((element: GraphNode) => element.id == event.node.id);
+
+    switch (event.node.type) {
+        case 'inputType':
+        case 'ruleIdentifier':
+        default:
+            nodeDataIndex.value = 'label';
+            break;
+    }
+
+    dialog.value?.open();
+};
 </script>
 
 <template>
-    <VueFlow
-        :node-types="nodeTypes"
-        v-model:nodes="nodes"
-        v-model:edges="edges"
-        :multi-selection-key-code="multiSelectKeys"
-    >
+    <VueFlow @connect="onConnect" @node-double-click="onNodeDoubleClick">
         <Background :variant="BackgroundVariant.Lines" :pattern-color="'#efefef'" :size="0.8" />
         <Controls />
         <Dialog ref="dialog" @close="">
@@ -38,6 +51,7 @@ nodes.value?.forEach(
                 <input
                     class="px-1 py-2 border border-black hover:border-blue-600 focus:border-transparent"
                     type="text"
+                    v-model.value="nodes[selectedNode].data[nodeDataIndex]"
                 />
             </template>
             <template v-slot:accept_button_text>Change Input</template>
