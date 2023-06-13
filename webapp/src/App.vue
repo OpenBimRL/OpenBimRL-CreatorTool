ba5364dd841fb4ae47caca280e8455fb39c0622a
Florian Becker <florian00m.becker@gmail.com>
2023-06-13 11:29:44 +0000

    refined overlays

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -4,16 +4,44 @@ import { Graph, Help, SideOverlay } from './components';
 import GraphNodeMenu from './components/graph/modals/GraphNodeMenu.vue';
 import Navigation from './components/navigation';
 
-const showHelp = ref(false);
-const showNodeLib = ref(false);
+enum Modal {
+    Help,
+    NodeLib,
+    SideOverlay,
+}
+const modals = ref<{ [key in Modal]?: boolean }>({});
+
+modals.value[Modal.Help] = false;
+modals.value[Modal.NodeLib] = false;
+
+const toggleModal = (modalName: Modal) => {
+    const oldState = modals.value[modalName];
+    closeAllModals();
+    modals.value[modalName] = !oldState;
+};
+
+const closeAllModals = () => {
+    for (const [, index] of Object.entries(Modal)) modals.value[index as Modal] = false;
+};
 </script>
 
 <template>
-    <Navigation @showHelp="showHelp = !showHelp" @showNodeLib="showNodeLib = !showNodeLib" />
-    <SideOverlay />
-    <Help v-show="showHelp" @close="showHelp = false" />
-    <GraphNodeMenu v-show="showNodeLib" />
-    <main>
+    <Navigation @showHelp="toggleModal(Modal.Help)" @showNodeLib="toggleModal(Modal.NodeLib)" />
+    <SideOverlay
+        :class="{ open: modals[Modal.SideOverlay] }"
+        @open="toggleModal(Modal.SideOverlay)"
+        @close="toggleModal(Modal.SideOverlay)"
+    />
+    <Help
+        :class="{ 'translate-x-full': !modals[Modal.Help] }"
+        class="transition-transform"
+        @close="toggleModal(Modal.Help)"
+    />
+    <GraphNodeMenu
+        :class="{ 'translate-x-full': !modals[Modal.NodeLib] }"
+        class="transition-transform"
+    />
+    <main @click="closeAllModals()">
         <Graph class="h-screen" />
     </main>
 </template>
--- webapp/src/components/SideOverlay.vue
+++ webapp/src/components/SideOverlay.vue
@@ -1,29 +1,26 @@
 <template>
     <aside
         class="fixed inset-y-16 right-4 z-10 w-10 rounded border bg-neutral-200 bg-opacity-90 shadow-md transition-all duration-200 selection:shadow-xl"
-        :class="{ open: open }"
     >
         <button
             class="relative h-full w-10 transition-colors duration-300 hover:bg-neutral-300"
-            @click="handler"
+            @click="$emit('open')"
         >
-            <div
-                class="absolute -left-2 block -rotate-90 text-xl"
-                v-text="open ? 'close' : 'open'"
-            />
+            <div class="absolute -left-2 block -rotate-90 text-xl" />
         </button>
     </aside>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const open = ref(false);
-
-const handler = (event: Event) => (open.value = !open.value);
-</script>
 <style scoped>
 aside.open {
     @apply w-1/2;
+}
+
+aside:not(.open) div::before {
+    content: 'open';
+}
+
+aside.open div::before {
+    content: 'close';
 }
 </style>
