6e25c7cbe35bd078913c78da6c95c842b663c8cb
Florian Becker <florian00m.becker@gmail.com>
2023-07-06 21:20:41 +0000

    added new types imports

--- webapp/src/components/model_check_view/Types.d.ts
+++ webapp/src/components/model_check_view/Types.d.ts
@@ -1,1 +1,1 @@
-export type { GraphInject, SubCheck, RulesOrRuleSets, ResultSet, CustomNode } from '../graph/Types';
+export type { GraphInject, SubCheck, RulesOrRuleSets, ResultSet, CustomNode, Rule, RuleSet } from '../graph/Types';
