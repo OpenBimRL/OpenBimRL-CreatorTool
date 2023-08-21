a83e50ae7ff88062502334779eb206ad4af3cae0
Florian Becker <florian00m.becker@gmail.com>
2023-08-21 13:59:26 +0000

    added global parser

--- webapp/src/keys.ts
+++ webapp/src/keys.ts
@@ -1,4 +1,7 @@
+import Parser from '@/ParserOpenBIMRL';
 import { InjectionKey } from 'vue';
 import { GraphInject } from './components/graph/Types';
 
+
 export const graphInjectionKey: InjectionKey<GraphInject> = Symbol('graph');
+export const parserInjectionKey: InjectionKey<Parser> = Symbol('parser')
--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import Parser from './ParserOpenBIMRL';
 import { initialGraph } from './components/graph/config';
-import { graphInjectionKey } from './keys';
+import { graphInjectionKey, parserInjectionKey } from './keys';
 import './style.css';
 
 const graph = initialGraph();
+const parser = new Parser();
 
 const app = createApp(App);
 app.provide(graphInjectionKey, graph);
+app.provide(parserInjectionKey, parser);
 app.mount('#app');
