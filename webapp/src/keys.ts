853e589554aad88b349197dfa83d32a963f8e3d7
Florian Becker <florian00m.becker@gmail.com>
2023-08-22 10:27:25 +0000

    added dark mode key

--- webapp/src/keys.ts
+++ webapp/src/keys.ts
@@ -1,7 +1,7 @@
 import Parser from '@/ParserOpenBIMRL';
-import { InjectionKey } from 'vue';
+import { InjectionKey, Ref } from 'vue';
 import { GraphInject } from './components/graph/Types';
 
-
 export const graphInjectionKey: InjectionKey<GraphInject> = Symbol('graph');
-export const parserInjectionKey: InjectionKey<Parser> = Symbol('parser')
+export const parserInjectionKey: InjectionKey<Parser> = Symbol('parser');
+export const darkModeKey: InjectionKey<Ref<boolean>> = Symbol('dark');
