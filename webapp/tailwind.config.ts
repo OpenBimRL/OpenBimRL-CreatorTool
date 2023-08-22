072fc439cceecd7f0afe7f3ce411ac90bff65381
Florian Becker <florian00m.becker@gmail.com>
2023-08-22 10:27:06 +0000

    changed tailwind colors

--- webapp/tailwind.config.ts
+++ webapp/tailwind.config.ts
@@ -1,20 +1,21 @@
+// @ts-ignore
 import colors from 'tailwindcss/colors';
 
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+    darkMode: 'class',
     theme: {
         extend: {
             colors: {
                 default: {
-                    overlay: colors.neutral[100],
-                    light: colors.neutral[300],
-                    contrast: colors.gray[300],
-                    dark: colors.gray[500],
+                    darkest: '#000000',
+                    dark: '#14213D',
+                    medium: colors.neutral[200],
+                    light: colors.teal[50],
+                    contrast: '#FCA311',
                 },
                 success: colors.green[500],
                 'ease-blue': colors.blue[600],
-                primary: colors.white,
-                secondary: colors.black,
                 danger: colors.red[600],
             },
         },
