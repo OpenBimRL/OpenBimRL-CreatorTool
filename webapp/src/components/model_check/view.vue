122b648dc3fc23eb12813a0a4e64db2ade86ad7b
Florian Becker <florian.becker-f86@ruhr-uni-bochum.de>
2023-08-29 09:27:27 +0000

    finalized model checking

--- webapp/src/components/model_check/view.vue
+++ webapp/src/components/model_check/view.vue
@@ -4,52 +4,46 @@
             <h1 class="text-2xl my-auto">Checks</h1>
         </div>
         <div style="grid-area: sidebar" class="p-2 mr-2 rounded-lg border select-none">
-            <Tree
-                :nodes="data"
-                :custom-styles="treeStyle"
-                :custom-options="treeOptions"
-                ref="tree"
-                @dblclick="doubleClickEvent"
-            />
+            <Tree ref="tree" :data="graph" @select="currentNode = $event" />
         </div>
         <div
             style="grid-area: main"
             class="relative overflow-hidden p-10 border rounded-lg bg-default-medium dark:bg-default-darkest dark:bg-opacity-50"
         >
-            <div v-if="currentRule">
+            <div v-if="currentNode && tree">
                 <p>
-                    <span v-for="(id, index) in tree.findNodePath(currentRule.id)">
+                    <span v-for="(id, index) in currentNode.path">
                         <span v-if="index != 0">&nbsp;/&nbsp;</span>
                         <!-- I've never seen more inefficient code... Well 'git blame Florian' -->
                         <button
-                            @mouseenter="tree.selectNode(id)"
-                            @mouseleave="tree.selectNode(currentRule.id)"
-                            @click="selectNode(tree.findNode(id))"
+                            @mouseenter="tree.nodeStateMap.get(id)!.state.hover = true"
+                            @mouseleave="tree.nodeStateMap.get(id)!.state.hover = false"
+                            @click="tree.selectNode(id)"
                         >
-                            {{ (tree.findNode(id) as TreeNode).text }}
+                            {{ tree.nodeStateMap.get(id)!.text }}
                         </button>
                     </span>
                 </p>
                 <hr class="border-default-dark" />
                 <!-- check wheather it's a rule or ruleset -->
-                <div v-if="currentRule.data?.type === RuleOrRuleSetType.RULESET">
-                    <RuleSetForm :rule-set="(currentRule.data as RuleSet)" />
+                <div v-if="currentNode.type === RuleOrRuleSetType.RULESET">
+                    <RuleSetForm :rule-set="(currentNode.data as RuleSet)" />
                 </div>
-                <div v-else-if="currentRule.data?.type === RuleOrRuleSetType.RULE">
-                    <RuleForm :rule="(currentRule.data as Rule)" />
+                <div v-else-if="currentNode.type === RuleOrRuleSetType.RULE">
+                    <RuleForm :rule="(currentNode.data as Rule)" />
                 </div>
-                <div v-else-if="currentRule.data?.type === 'resultSet'">
+                <div v-else-if="currentNode.data?.type === 'resultSet'">
                     <ResultSetForm
-                        :result-set="(currentRule.data as ResultSet)"
+                        :result-set="(currentNode.data as ResultSet)"
                         :graph-nodes="graph.elements"
                         :sub-checks="graph.subChecks"
                     />
                 </div>
-                <div v-else-if="currentRule.data?.hasOwnProperty('name')">
+                <div v-else-if="currentNode.data?.hasOwnProperty('name')">
                     <p class="my-1">
                         <span class="text-xl">Edit Name</span>
                     </p>
-                    <InputField class="my-1" v-model="(currentRule.data as SubCheck).name">
+                    <InputField class="my-1" v-model="(currentNode.data as SubCheck).name">
                         <span>Name</span>
                     </InputField>
                 </div>
@@ -62,51 +56,19 @@
 
 <script setup lang="ts">
 import { graphInjectionKey } from '@/keys';
-import { Ref, inject, reactive, ref } from 'vue';
+import { inject, ref } from 'vue';
 import type { GraphInject, ResultSet, Rule, RuleSet, SubCheck } from '../graph/Types';
-import type { TreeNode } from './Tree';
-import { treeOptions as treeOptionsFunc, treeStyle } from './TreeConfig';
-import { convertResultSets, convertSubChecks } from './TreeConverter';
-import { ResultSetForm, RuleForm, RuleSetForm } from './forms';
-
-// @ts-ignore
-import Tree from 'vuejs-tree';
-import InputField from '../InputField.vue';
 import { RuleOrRuleSetType } from '../graph/enums';
-
-const currentRule = ref<TreeNode | null>(null);
-
+import { ResultSetForm, RuleForm, RuleSetForm } from './forms';
+import Tree from './tree';
+import { ITree } from './tree/Tree';
+import { TreeNode } from './tree/Types';
+import { InputField } from '..';
 const { graph } = inject(graphInjectionKey) as GraphInject;
 
-const data: Ref<Array<TreeNode>> = ref<Array<TreeNode>>([] as Array<TreeNode>);
-data.value.push(
-    ...convertSubChecks(graph.value.subChecks),
-    ...convertResultSets(graph.value.resultSets),
-);
-const tree = ref<any>(null);
-
-const doubleClickEvent = (event: MouseEvent) => {
-    const node = tree.value.findNode(
-        (event.target as HTMLElement).closest('li')?.getAttribute('data-id'),
-    ) as TreeNode | null;
-
-    if (!node) return;
-
-    node.state.expanded = !node.state.expanded;
-
-    selectNode(node);
-};
-
-const selectNode = (node: TreeNode) => {
-    if (node.selectable === false) return;
-    tree.value.selectNode(node.id);
-
-    currentRule.value = reactive(node);
-};
-
-const treeOptions = treeOptionsFunc(currentRule, tree);
+const tree = ref<ITree | null>(null);
+const currentNode = ref<TreeNode | null>(null);
 </script>
-
 <style scoped>
 div.grid {
     grid-template-areas:
