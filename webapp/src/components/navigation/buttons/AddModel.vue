cb08b44f0931b9e3430a45a981a27ebaa6362ebb
Florian Becker <florian.becker-f86@ruhr-uni-bochum.de>
2023-12-22 13:05:42 +0000

    added new button component

---
+++ webapp/src/components/navigation/buttons/AddModel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex">
+        <input
+            class="hidden"
+            type="file"
+            accept=".ifc"
+            ref="ifcModelInput"
+            @change="addModelFunc"
+        />
+        <button class="nav-button" @click="ifcModelInput?.click()">Add Model</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { addModel } from '@/modules/apiConnection';
+import { Ref, ref } from 'vue';
+
+const ifcModelInput: Ref<HTMLInputElement | null> = ref(null);
+const addModelFunc = (event: Event) => {
+    const files = ifcModelInput.value?.files;
+    if (!files || !files[0]) return;
+    addModel(files[0]);
+};
+</script>
+
+<style scoped></style>
