d6dc0bd8ed8613e5e6358272053c17e0b4ab15e5
Florian Becker <florian.becker-f86@ruhr-uni-bochum.de>
2024-01-05 21:29:02 +0000

    removed obsolete exports

--- webapp/src/modules/index.ts
+++ webapp/src/modules/index.ts
@@ -1,4 +1,3 @@
 export { isConnected as apiIsConnected } from './apiConnection';
 export { default as darkMode } from './darkmode';
 export { default as router } from './router';
-export { models as ifcModels, getSelected as getSelectedModel } from "./ifcModels";
