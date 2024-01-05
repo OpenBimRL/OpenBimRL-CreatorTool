63302ae2fe3c976e7d8eac170be170c5e0ed8d13
Florian Becker <florian.becker-f86@ruhr-uni-bochum.de>
2024-01-05 12:49:44 +0000

    updated API connection

--- webapp/src/modules/apiConnection.ts
+++ webapp/src/modules/apiConnection.ts
@@ -1,4 +1,6 @@
-export const apiEndpoint = 'http://localhost:8080';
+import { ref } from "vue";
+
+export const apiEndpoint = ref(new URL('http://localhost:8080'));
 
 interface ApiAnswer<T> {
     status: string;
@@ -11,7 +13,7 @@ interface ApiPostData<T> {
 
 export async function isConnected(): Promise<boolean> {
     try {
-        const response = await getApi<ApiAnswer<boolean>>('/connection');
+        const response = await getApi<boolean>('/connection');
         return response.content;
     } catch (e) {
         console.error(e);
@@ -19,7 +21,7 @@ export async function isConnected(): Promise<boolean> {
     }
 }
 
-export async function addModel(file: File): Promise<string> {
+export async function addModel(file: File): Promise<ApiAnswer<string>> {
     return await postApi<string>("/model", {data: await file.text()})
 }
 
@@ -27,24 +29,24 @@ export async function getModel(id: string): Promise<Uint8Array> {
     return await getApiBinary("/model/" + id);
 }
 
-async function getApi<T>(url: string): Promise<T> {
-    const response = await fetch(apiEndpoint + url);
+async function getApi<T>(url: string): Promise<ApiAnswer<T>> {
+    const response = await fetch(apiEndpoint.value + url);
     if (!response.ok) throw new Error(response.statusText);
 
-    const data = await (response.json() as Promise<T>);
+    const data = await (response.json() as Promise<ApiAnswer<T>>);
     return data;
 }
 
 async function getApiBinary(url: string): Promise<Uint8Array> {
-    const response = await fetch(apiEndpoint + url);
+    const response = await fetch(apiEndpoint.value + url);
     if (!response.ok) throw new Error(response.statusText);
 
     const buffer = await response.arrayBuffer();
     return new Uint8Array(buffer);
 }
 
-async function postApi<T>(url: string, params: ApiPostData<any>): Promise<T> {
-    const response = await fetch(apiEndpoint + url, {
+async function postApi<T>(url: string, params: ApiPostData<any>): Promise<ApiAnswer<T>> {
+    const response = await fetch(apiEndpoint.value + url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -53,6 +55,6 @@ async function postApi<T>(url: string, params: ApiPostData<any>): Promise<T> {
     });
     if (!response.ok) throw new Error(response.statusText);
 
-    const data = await (response.json() as Promise<T>);
+    const data = await (response.json() as Promise<ApiAnswer<T>>);
     return data;
 }
