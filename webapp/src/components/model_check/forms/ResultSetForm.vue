93a1bbc973ea36927e4e1d5d7dfb576c2051a27d
Florian Becker <florian.becker-f86@ruhr-uni-bochum.de>
2024-05-02 15:54:43 +0000

    adhere to eslint rules

--- webapp/src/components/model_check/forms/ResultSetForm.vue
+++ webapp/src/components/model_check/forms/ResultSetForm.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/no-mutating-props -->
 <template>
     <div class="result_set-form">
         <p><span class="text-xl"> Edit RuleSet </span></p>
@@ -9,21 +10,15 @@
             <div class="flex w-full rounded overflow-hidden border dark:border-default-medium">
                 <label
                     class="p-2 text-sm cursor-text select-none bg-default-light border-r border-inherit dark:bg-default-dark"
-                    for="rule-form-operator-select"
-                >
+                    for="rule-form-operator-select">
                     <span>Elements</span>
                 </label>
-                <select
-                    v-model="resultSet.elements"
+                <select v-model="resultSet.elements"
                     class="w-full pl-1 bg-default-light dark:bg-default-dark focus-visible:outline-none"
-                    id="rule-form-operator-select"
-                >
-                    <option
-                        v-for="value in graphNodes.filter(
-                            element => element.type === 'ruleIdentifier',
-                        )"
-                        :value="value.data?.label"
-                    >
+                    id="rule-form-operator-select">
+                    <option v-for="value in graphNodes.filter(
+            element => element.type === 'ruleIdentifier',
+        )" :value="value.data?.label" :key="value.id">
                         {{ value.data?.label }}
                     </option>
                 </select>
@@ -31,16 +26,15 @@
             <div class="flex w-full rounded overflow-hidden border dark:border-default-medium">
                 <label
                     class="p-2 text-sm cursor-text select-none bg-default-light border-r border-inherit dark:bg-default-dark"
-                    for="rule-form-operator-select"
-                >
+                    for="rule-form-operator-select">
                     <span>Filter</span>
                 </label>
-                <select
-                    v-model="resultSet.filter"
+                <select v-model="resultSet.filter"
                     class="w-full pl-1 bg-default-light dark:bg-default-dark focus-visible:outline-none"
-                    id="rule-form-operator-select"
-                >
-                    <option v-for="value in filter" :value="value.label">{{ value.label }}</option>
+                    id="rule-form-operator-select">
+                    <option v-for="(value, index) in filter" :value="value.label" :key="index">
+                        {{ value.label }}
+                    </option>
                 </select>
             </div>
         </div>
@@ -94,7 +88,7 @@ const filterRecursive = (rulesOrRuleSets: RulesOrRuleSets): Array<Rule> => {
 </script>
 
 <style scoped>
-div.result_set-form > * {
+div.result_set-form>* {
     @apply my-1;
 }
 </style>
--- webapp/src/components/model_check/forms/RuleForm.vue
+++ webapp/src/components/model_check/forms/RuleForm.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/no-mutating-props -->
 <template>
     <div class="rule-form">
         <p>
@@ -10,16 +11,13 @@
             <div class="flex w-full rounded overflow-hidden border dark:border-default-medium">
                 <label
                     class="p-2 text-sm cursor-text select-none bg-default-light border-r border-inherit dark:bg-default-dark"
-                    for="rule-form-operator-select"
-                >
+                    for="rule-form-operator-select">
                     <span>Quantifier</span>
                 </label>
-                <select
-                    v-model="rule.quantifier"
+                <select v-model="rule.quantifier"
                     class="w-full pl-1 bg-default-light dark:bg-default-dark focus-visible:outline-none"
-                    id="rule-form-operator-select"
-                >
-                    <option v-for="(value, name) in RuleQuantifier" :value="value">
+                    id="rule-form-operator-select">
+                    <option v-for="(value, name) in RuleQuantifier" :value="value" :key="value">
                         {{ name }}
                     </option>
                 </select>
@@ -27,16 +25,15 @@
             <div class="flex w-full rounded overflow-hidden border dark:border-default-medium">
                 <label
                     class="p-2 text-sm cursor-text select-none bg-default-light border-r border-inherit dark:bg-default-dark"
-                    for="rule-form-operator-select"
-                >
+                    for="rule-form-operator-select">
                     <span>Operator</span>
                 </label>
-                <select
-                    v-model="rule.operator"
+                <select v-model="rule.operator"
                     class="w-full pl-1 bg-default-light dark:bg-default-dark focus-visible:outline-none"
-                    id="rule-form-operator-select"
-                >
-                    <option v-for="(value, name) in RuleOperator" :value="value">{{ name }}</option>
+                    id="rule-form-operator-select">
+                    <option v-for="(value, name) in RuleOperator" :value="value" :key="value">
+                        {{ name }}
+                    </option>
                 </select>
             </div>
         </div>
@@ -57,11 +54,11 @@ import InputField from '@/components/InputField.vue';
 import { Rule } from '@/components/graph/Types';
 import { RuleOperator, RuleQuantifier } from '@/components/graph/enums';
 
-const props = defineProps<{ rule: Rule }>();
+defineProps<{ rule: Rule }>();
 </script>
 
 <style scoped>
-div.rule-form > * {
+div.rule-form>* {
     @apply my-1;
 }
 </style>
