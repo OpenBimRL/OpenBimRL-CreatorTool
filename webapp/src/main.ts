7a5c31fa944a2e12d8a0b1f78c075052e3fba0c8
Florian Becker <florian.becker-f86@ruhr-uni-bochum.de>
2025-03-25 15:10:43 +0000

    provided darkmode appwide

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,17 +1,19 @@
-import { createApp } from 'vue';
+import { createApp, ref } from 'vue';
 import App from './App.vue';
 import Parser from './ParserOpenBIMRL';
 import { initialGraph } from './components/graph/config';
-import { darkModeKey, graphInjectionKey, parserInjectionKey } from './keys';
+import { apiConnectionInjectionKey, darkModeKey, graphInjectionKey, parserInjectionKey } from './keys';
 import { darkMode, router } from './modules';
 import './style.css';
 
 const graph = initialGraph();
 const parser = new Parser();
+const apiConnection = ref<boolean | undefined>(undefined);
 
 const app = createApp(App);
 app.provide(graphInjectionKey, graph);
 app.provide(parserInjectionKey, parser);
 app.provide(darkModeKey, darkMode);
+app.provide(apiConnectionInjectionKey, apiConnection);
 app.use(router);
 app.mount('#app');
